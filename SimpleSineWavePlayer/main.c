1aee1c0966de15d6b888a105aab3ba9b1de4569b
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-25 08:37:49 +0300

    Initial Commit

---
+++ SimpleSineWavePlayer/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SimpleSineWavePlayer
+//
+//  Created by Panayotis Matsinopoulos on 25/7/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
